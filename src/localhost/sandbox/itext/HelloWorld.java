7ce6a94466ef10de117f676aa5e615809087f894
massuda <massuda.marcelo@gmail.com>
2015-05-24 18:22:21 -0300

    Set page size to A4

--- src/localhost/sandbox/itext/HelloWorld.java
+++ src/localhost/sandbox/itext/HelloWorld.java
@@ -1,10 +1,31 @@
 package localhost.sandbox.itext;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException, DocumentException {
 
+		// adapted from "iText in Action"
+
+		System.out.println("start...");
+		// step 1
+		Document document = new Document();
+		// step 2
+		PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+		// step 3
+		document.open();
+		// step 4
+		document.add(new Paragraph("Hello World!"));
+		// step 5
+		document.close();
+
+		System.out.println("finish");
 	}
-
 }
